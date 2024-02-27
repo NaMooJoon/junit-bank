b49e69a00b80288217b910869aacae25defbd043
Kim JunHyun <jjag1015@naver.com>
2024-02-28 01:51:06 +0900

    feat(회원가입): 회원가입 controller 정규식표현 dto에 적용

--- src/main/java/shop/mtcoding/junitbank/dto/user/UserRequestDto.java
+++ src/main/java/shop/mtcoding/junitbank/dto/user/UserRequestDto.java
@@ -1,6 +1,8 @@
 package shop.mtcoding.junitbank.dto.user;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,13 +14,20 @@ public class UserRequestDto {
     @Getter
     @Setter
     public static class JoinRequestDto {
+        // 영문, 숫자는 되고, 길이 2~20자
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해 주세요")
         @NotEmpty
         private String username;
+
+        // 길이 4 ~ 20
         @NotEmpty
+        @Size(min = 4, max = 20)
         private String password;
         @NotEmpty
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,6}@[a-zA-Z0-9]{2,8}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해 주세요")
         private String email;
         @NotEmpty
+        @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요")
         private String fullname;
 
         public User toEntity(BCryptPasswordEncoder passwordEncoder) {
---
+++ src/test/java/shop/mtcoding/junitbank/temp/RegexTest.java
@@ -0,0 +1,118 @@
+package shop.mtcoding.junitbank.temp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.regex.Pattern;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RegexTest {
+
+    @Test
+    void 한글만_된다() {
+        final String REGEX = "^[ㄱ-ㅎㅏ-ㅣ가-힣]+$";
+        String value1 = "가나";
+        String value2 = "가나12";
+        boolean result1 = Pattern.matches(REGEX, value1);
+        boolean result2 = Pattern.matches(REGEX, value2);
+
+
+        assertThat(result1).isEqualTo(true);
+        assertThat(result2).isEqualTo(false);
+    }
+
+    @Test
+    void 한글은_안된다() {
+        final String REGEX = "^[^ㄱ-ㅎㅏ-ㅣ가-힣]+$";
+        String value1 = "";
+        String value2 = "ab12";
+        boolean result1 = Pattern.matches(REGEX, value1);
+        boolean result2 = Pattern.matches(REGEX, value2);
+
+        assertThat(result1).isEqualTo(false);
+        assertThat(result2).isEqualTo(true);
+    }
+
+    @Test
+    void 영어만_된다() {
+        final String REGEX = "^[a-zA-Z]+$";
+        String value1 = "ssar";
+        String value2 = "ab12";
+        boolean result1 = Pattern.matches(REGEX, value1);
+        boolean result2 = Pattern.matches(REGEX, value2);
+
+        assertThat(result1).isEqualTo(true);
+        assertThat(result2).isEqualTo(false);
+    }
+
+    @Test
+    void 영어는_안된다() {
+        final String REGEX = "^[^a-zA-Z]+$";
+        String value1 = "ssar";
+        String value2 = "가12";
+        boolean result1 = Pattern.matches(REGEX, value1);
+        boolean result2 = Pattern.matches(REGEX, value2);
+
+        assertThat(result1).isEqualTo(false);
+        assertThat(result2).isEqualTo(true);
+    }
+
+    @Test
+    void 영어와_숫자만_된다() {
+        final String REGEX = "^[a-zA-Z0-9]+$";
+        String value1 = "ssar11";
+        String value2 = "가12";
+        boolean result1 = Pattern.matches(REGEX, value1);
+        boolean result2 = Pattern.matches(REGEX, value2);
+
+        assertThat(result1).isEqualTo(true);
+        assertThat(result2).isEqualTo(false);
+    }
+
+    @Test
+    void 영어만_되고_길이는_최소2_최대4_이다() {
+        final String REGEX = "^[a-zA-Z]{2,4}$";
+        String value1 = "ssar";
+        String value2 = "ab";
+        String value3 = "absss";
+        boolean result1 = Pattern.matches(REGEX, value1);
+        boolean result2 = Pattern.matches(REGEX, value2);
+        boolean result3 = Pattern.matches(REGEX, value3);
+
+        assertThat(result1).isEqualTo(true);
+        assertThat(result2).isEqualTo(true);
+        assertThat(result3).isEqualTo(false);
+    }
+
+    @Test
+    void user_username_test() {
+        String username = "ssar";
+        String username2 = "ssar가";
+        boolean result = Pattern.matches("^[a-zA-Z0-9]{2,20}$", username);
+        boolean result2 = Pattern.matches("^[a-zA-Z0-9]{2,20}$", username2);
+        assertThat(result).isEqualTo(true);
+        assertThat(result2).isEqualTo(false);
+    }
+
+    @Test
+    void user_fullname_test() {
+        String username = "ssar";
+        String username2 = "ssar가";
+        boolean result = Pattern.matches("^[a-zA-Z가-힣]{1,20}$", username);
+        boolean result2 = Pattern.matches("^[a-zA-Z가-힣]{1,20}$", username2);
+        assertThat(result).isEqualTo(true);
+        assertThat(result2).isEqualTo(true);
+    }
+
+    @Test
+    void user_email_test() {
+        final String REGEX = "^[a-zA-Z0-9]{2,6}@[a-zA-Z0-9]{2,8}\\.[a-zA-Z]{2,3}$";
+        String username = "ssar@....";
+        String username2 = "ssar@naver.com";
+        boolean result = Pattern.matches(REGEX, username);
+        boolean result2 = Pattern.matches(REGEX, username2);
+        assertThat(result).isEqualTo(false);
+        assertThat(result2).isEqualTo(true);
+    }
+}
